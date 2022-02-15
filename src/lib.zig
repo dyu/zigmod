7b67e6dce0e4274ccf6fe6641a8070d6c105b2af
dyu <david.yu.ftw@gmail.com>
2022-02-16 05:53:37 +0800

    segregate commands

--- src/lib.zig
+++ src/lib.zig
@@ -6,12 +6,16 @@ pub const commands_to_bootstrap = struct {
     pub const ci = @import("./cmd/ci.zig");
 };
 
-pub const commands = struct {
-    usingnamespace commands_to_bootstrap;
+pub const commands_core = struct {
     pub const init = @import("./cmd/init.zig");
     pub const sum = @import("./cmd/sum.zig");
-    pub const zpm = @import("./cmd/zpm.zig");
     pub const license = @import("./cmd/license.zig");
+};
+
+pub const commands = struct {
+    usingnamespace commands_to_bootstrap;
+    usingnamespace commands_core;
+    pub const zpm = @import("./cmd/zpm.zig");
     pub const aq = @import("./cmd/aq.zig");
 };
 
